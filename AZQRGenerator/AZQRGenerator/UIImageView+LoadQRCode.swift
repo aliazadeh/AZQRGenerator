6c07ba3b5d214af2a45e1aaf5e547d6228e43dea
Ali Azadeh <ali.azadeh@outlook.com>
2023-03-07 08:47:48 -0800

    Make it compatible with Swift version 5

--- AZQRGenerator/AZQRGenerator/UIImageView+LoadQRCode.swift
+++ AZQRGenerator/AZQRGenerator/UIImageView+LoadQRCode.swift
@@ -15,7 +15,7 @@ extension UIImageView {
         if img != nil {
             let scaleX = self.frame.size.width / (img!.extent.size.width)
             let scaleY = self.frame.size.height / (img!.extent.size.height)
-             let transformedImage = img!.applying(CGAffineTransform(scaleX: scaleX, y: scaleY))
+            let transformedImage = img!.transformed(by: CGAffineTransform(scaleX: scaleX, y: scaleY))
         self.image = UIImage(ciImage: transformedImage)
         }
     }
